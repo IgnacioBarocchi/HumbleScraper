ce421ad2c711559b7c73b9804a16c9eea6a6d039
Ignacio Barocchi <barocchi.ignacio@gmail.com>
2020-06-14 17:51:14 -0300

    Url generator

---
+++ index.ts
@@ -0,0 +1,33 @@
+
+let requestedUrl: string;
+const clientUI = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const urlGen: {
+  baseUrl: string;
+  path: {
+    format: string;
+    build: any;
+  };
+  words: string[];
+} = {
+  baseUrl: "https://es.wikipedia.org/",
+  path: {
+    format: "/wiki/{{word}}",
+    build: function (word: string) {
+      return urlGen.baseUrl + this.format.replace("{{word}}", word);
+    },
+  },
+  words: ["Zapato", "Pato", "Pirámide"],
+};
+
+clientUI.question("Choose a word >", function (userInput) {
+  urlGen.words.push(userInput.trim());
+  requestedUrl = encodeURI(
+    urlGen.path.build(urlGen.words[urlGen.words.length - 1])
+  );
+  console.log(requestedUrl);
+});
+
