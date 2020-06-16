8e27e7e436b060749d70f2296a2de625c499c5ca
Ignacio Barocchi <barocchi.ignacio@gmail.com>
2020-06-16 14:20:18 -0300

    Type error

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { urlBuilder } from "./urlBuilder";
+import { multiUrlGenerator } from "./multiUrlGenerator";
 
 const clientUI = require("readline").createInterface({
   input: process.stdin,
@@ -6,7 +6,7 @@ const clientUI = require("readline").createInterface({
 });
 
 clientUI.question("args:baseUrl,route >", function (userInput: string) {
-  const args: string[] = userInput.split(",");
-  const requestedUrl = urlBuilder(args[0].trim(), encodeURI(args[1].trim()));
+  const args: any[] = userInput.split(",");/* string and obj */
+  const requestedUrl = multiUrlGenerator(args[0].trim(), args[1]);
   console.log(requestedUrl);
 });
