b0e3d6257bb45445b9e9060367d189f445bba232
Ignacio Barocchi <barocchi.ignacio@gmail.com>
2020-06-16 14:21:04 -0300

    Type error

--- index.ts
+++ index.ts
@@ -7,6 +7,6 @@ const clientUI = require("readline").createInterface({
 
 clientUI.question("args:baseUrl,route >", function (userInput: string) {
   const args: any[] = userInput.split(",");/* string and obj */
-  const requestedUrl = multiUrlGenerator(args[0].trim(), args[1]);
-  console.log(requestedUrl);
+  // const requestedUrl = multiUrlGenerator(args[0].trim(), args[1]);
+  // console.log(requestedUrl);
 });
