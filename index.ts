6021ac59081412017d882a664cc39f030dc0204e
Ignacio Barocchi <barocchi.ignacio@gmail.com>
2020-06-17 00:31:58 -0300

    Add arrayOfObjectsFactory function

--- index.ts
+++ index.ts
@@ -1,8 +1,5 @@
 import { multiUrlGenerator } from "./multiUrlGenerator";
-type UserObject = {
-  $home: string;
-  $name: string;
-};
+
 const clientUI = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -23,10 +20,22 @@ class Url {
   }
 }
 
+function arrayOfObjectsFactory(website: string[], path: string[]) {
+  let voidArray = [];
+  for (let i = 0; i < website.length; i++) {
+    let obj: {} = new Url(website[i], path[i]).createObject();
+    voidArray.push(obj);
+  }
+  return voidArray;
+}
+
 clientUI.question("type <website>, <routes> => ", function (userInput: string) {
   const userArray: string[] = userInput.split(",");
-  const newUrl = new Url(userArray[0], userArray[1]);
-  const requestedUrl = multiUrlGenerator("http://$home/$name", [newUrl.createObject()]);
+  let website: string[] = userArray.filter((a, i) => i % 2 === 0);
+  let path: string[] = userArray.filter((a, i) => i % 2 !== 0);
+  const requestedUrl = multiUrlGenerator(
+    "http://$home/$name",
+    arrayOfObjectsFactory(website, path)
+  );
   console.log(requestedUrl);
-
 });
