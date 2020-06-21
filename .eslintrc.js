d2e9902673bada2d23fc55bc1756dbaf75491d96
scvsoft-miguelbarocchi <miguel.barocchi@quizlet.com>
2020-06-21 18:25:11 -0300

    Remove unnecesary copypasta

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,7 @@ module.exports = {
       jsx: true, // Allows for the parsing of JSX
     },
   },
-  settings: {
-    react: {
-      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
-  },
+  settings: {},
   extends: [
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
