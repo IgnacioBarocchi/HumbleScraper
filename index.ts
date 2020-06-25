18c5fe3df9727a8b51fd4a8b59660606c807d4c3
Ignacio Barocchi <barocchi.ignacio@gmail.com>
2020-06-25 16:27:03 -0300

    Add client

--- index.ts
+++ index.ts
@@ -1,17 +1,28 @@
 import { multiUrlGenerator } from './lib/multiUrlGenerator';
 import fetch, { Response } from 'node-fetch';
 import console from 'console';
-import { argv } from 'yargs';
+import readline from 'readline';
 const URL_CONFIG_PATH = './config/url_components';
 const PARSER_PATH = './config/parsers/';
 type ScraperMode = 'wikipedia' | 'mojang';
 const MODES: ReadonlyArray<ScraperMode> = ['wikipedia', 'mojang'];
-const MODE = argv.mode as ScraperMode;
 
-if (!MODES.includes(MODE as ScraperMode)) {
-  console.error(`Specified scraper mode "${MODE}" not found in config files.`);
-  process.exit(0);
-}
+const client = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+client.question('Choose the mode you want to run the program ', (MODE) => {
+  //TODO: change the question method to setPrompt()/popPromt() or reformulate the question() string to ask an actual question.
+  if (!MODES.includes(MODE as ScraperMode)) {
+    console.error(
+      `Specified scraper mode "${MODE}" not found in config files.`
+    );
+    process.exit(0);
+  }
+  main(MODE);
+  client.close();
+});
 
 async function getUrls(configFileName: string): Promise<string[]> {
   const urlConfig = await import(`${URL_CONFIG_PATH}/${configFileName}`);
@@ -38,5 +49,3 @@ async function main(mode: string) {
   }
   process.exit(0);
 }
-
-main(MODE);
