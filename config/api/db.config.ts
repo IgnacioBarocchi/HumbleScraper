a88ee4b4b71ce599cd720e25fc66073522dbf5b4
Ignacio Barocchi <barocchi.ignacio@gmail.com>
2020-07-03 12:38:10 -0300

    Add .env

--- config/api/db.config.ts
+++ config/api/db.config.ts
@@ -1,11 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
 export const firebaseConfig = {
-  apiKey: 'AIzaSyC3F48JASbrGz0Rkp-1u3dBuUhHgtpbFfI',
-  authDomain: 'etymon-ecd27.firebaseapp.com',
-  databaseURL: 'https://etymon-ecd27.firebaseio.com',
-  projectId: 'etymon-ecd27',
-  storageBucket: 'etymon-ecd27.appspot.com',
-  messagingSenderId: '463531942651',
-  appId: '1:463531942651:web:bc6c120b6122c957e4c65e',
-  measurementId: 'G-JC4Z11489D',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 //We could import and object from an environment with the keys.
